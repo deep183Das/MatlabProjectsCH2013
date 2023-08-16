8569b464054423726e5b6fd490f3f8c2cf9c688e
Deepanjhan Das <136880585+deep183Das@users.noreply.github.com>
2023-08-17 03:33:56 +0530

    Update Question3_b.m

--- ProblemSheet2/Question3_b.m
+++ ProblemSheet2/Question3_b.m
@@ -1,16 +1,16 @@
-%% Question_3_b
+%% The Colebrook and White equation : Question 3_b
 
 % Given parameters
 friction_factor = 0.0335;
-epsilon = 0.5e-3;  % Convert mm to meters
+epsilon = 0.5e-3;  % Converting mm to meters
 hydraulic_diameter = 0.1;
 
 % Initial guess for Reynolds number
 reynolds_initial = 10000;
 
-% Set options for fsolve
+% Setting options for fsolve
 options = optimset('fsolve');
-options.Display = 'off';  % Turn off display for fsolve
+options.Display = 'off';  % Turning off display for fsolve
 
 % Defining the function for fsolve
 func = @(reynolds) 1 / sqrt(friction_factor) + 2 * log10(epsilon / (3.71 * hydraulic_diameter) + 2.51 / (reynolds * sqrt(friction_factor)));
