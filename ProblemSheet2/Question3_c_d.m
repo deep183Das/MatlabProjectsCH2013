967565c85ea66ca688e4b07fb92f7ce515ef11bb
Deepanjhan Das <136880585+deep183Das@users.noreply.github.com>
2023-08-17 03:27:48 +0530

    Update Question3_c_d.m

--- ProblemSheet2/Question3_c_d.m
+++ ProblemSheet2/Question3_c_d.m
@@ -1,28 +1,28 @@
-%% Question 3_c_d
+%% The Colebrook and White equation : Question 3_c_d
 
 f_prev = linspace(0.0376, 0.0305, 10);
-epsilon = 0.5/1000; % all in meters
+% all in meters
+epsilon = 0.5/1000; 
 Dh = 0.1;
 
-% initial guess value for Re
-re_initial = 1000;
-% no. of iterations
-iter = 0;
-max_iteration = 1000;
-% tolerance value
-tol = 10^-4;
+re_initial = 1000;    % initial guess value for Re
+iter = 0;    % no. of iterations
+max_iteration = 1000; 
+tol = 10^-4;    % tolerance value
 error = 1;
 
 while((error > tol) & (iter < max_iteration))
 
     iter = iter + 1;
     Re = re_initial;
+    % using Newton-Raphson formula here
     re_initial = Re - ((((-2).*log10((epsilon./(3.71.*Dh)) + (2.51./(Re.*sqrt(f_prev))))) - (1./sqrt(f_prev))) ./ (f_prev.*(-1.5)).*(0.5 + (2.51./(Re.*log(10).*((epsilon./(3.71.*Dh)) + (2.51./(Re.*sqrt(f_prev))))))));
 
     error = abs(re_initial - Re);
 end
 
 rearr = re_initial;
+
 
 %% corresponding plot for friction_factor vs reynold number 
 plot(rearr, f_prev, LineStyle="-", Color='r', Marker='o', MarkerFaceColor='b');
