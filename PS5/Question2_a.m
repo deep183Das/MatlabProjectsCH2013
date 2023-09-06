28fd48cc6a0e4fda857e51896a75f63be3c1d1ac
Deepanjhan Das <136880585+deep183Das@users.noreply.github.com>
2023-09-06 18:34:14 +0530

    Question2_a.m

--- PS5/Question2_a.m
+++ PS5/Question2_a.m
@@ -1,11 +1,15 @@
 %% PS5 Question 2a
 
+% initial guess value
 x0 = [-1 -2];
 
-options = optimset('Maxiter',10,'TolFun',1e-6,'Display','Iter');
+% defining options for fsolve specifically
+options = optimset('MaxIter',10, 'TolFun',1e-6, 'Display','Iter');
 
+% The following order is important
 [x, fval, exitflag, output, jacobian] = fsolve(@equations, x0, options);
 
+% Extracting the number of iterations
 iterations = output.iterations;
 
 solution1 = struct('converged_x',x(1),'converged_y',x(2),'first_function_value',fval(1),'second_function_value',fval(2),'jacobian_convergence_value',jacobian,'exit_flag',exitflag,'number_of_iterations',iterations);
