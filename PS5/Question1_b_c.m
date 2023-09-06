a4c2e48f0039657940e48c01646788f4898aaf19
Deepanjhan Das <136880585+deep183Das@users.noreply.github.com>
2023-09-06 18:29:51 +0530

    Question1_b_c.m

--- PS5/Question1_b_c.m
+++ PS5/Question1_b_c.m
@@ -10,7 +10,7 @@ T = [300 1000]; % in K
 k0 = [0.001 0.34];
 Eact = [12000 5600];
 
-% defining field names as 'pre_exponential_factor' and 'activation_energy' 
+% defining field names as 'pre_exponential_factor' and 'activation_energy' of the structure array "reactiondata"
 reactiondata.pre_exponential_factor = k0;
 reactiondata.activation_energy = Eact;
 
@@ -19,7 +19,6 @@ rate = [r(1), r(2)];
 
 function r = reactionrate1(T,C1,C2,reactiondata)
     R = 8.314;
-
     r1 = (reactiondata.pre_exponential_factor(1))*(exp((-reactiondata.activation_energy(1))/(R*T(1))))*C1*C2;
     r2 = (reactiondata.pre_exponential_factor(2))*(exp((-reactiondata.activation_energy(2))/(R*T(2))))*C1*C2;
     r = [r1, r2];
