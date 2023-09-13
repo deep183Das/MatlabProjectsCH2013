7c719e821b84e49f4f9b48082e69e7c5fbdbeef4
Deepanjhan Das <136880585+deep183Das@users.noreply.github.com>
2023-09-13 20:17:14 +0530

    Question1.m

--- ProblemSheet6/Question1.m
+++ ProblemSheet6/Question1.m
@@ -3,15 +3,15 @@
 % a>
 p1 = @(x) 4*(x^4) + (3.2)*(x^2) - 4;
 p2 = @(x) (4.6)*(x^3) - 12*(x^2) + 10;
-p3 = @(x) (x-1)*((x-3)^2);      % p3 = x^3 - 7x^2 + 15x - 9
+p3 = @(x) (x-1)*((x-3)^2);      % p3 = x^3 - 7x^2 + 15x - 9 (after expanding)
 
 % b> values of p1,p2,p3 at x = 4.5
 value1 = p1(4.5);
 value2 = p2(4.5);
 value3 = p3(4.5);
 
-% c>
-p1_coeff = [4 0 3.2 0 -4];
+% c> using roots() to get all the root values (even the complex roots also)
+p1_coeff = [4 0 3.2 0 -4];  % can use both row & column vector
 r1 = roots(p1_coeff);   % roots of p1
 
 p2_coeff = [4.6 -12 0 10];
